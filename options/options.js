ff92707d7d3b92d6de5931091ab4570e0b6786ef
Crowdbotics <team@crowdbotics.com>
2023-12-18 07:50:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-rice-45071",
+  url: "https://round-rice-45071.botics.co",
+  api: "https://round-rice-45071.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
